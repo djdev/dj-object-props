55dbed2a82f5a87c26296e9698d68b7cc3d0864e
djdev <deepjyoti_rajkhowa@yahoo.com>
2024-01-01 19:15:42 -0500

    Update component details and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './style.css';
 
+const USER_DATA = {
+  name: "DJ Rajkhowa",
+  img: "https://avatars.githubusercontent.com/u/2480220?s=400&u=7737ef7a910c97a14a25f1ebac474d99fa467a02&v=4",
+  handle: "djdev"
+};
+
+function Badge({user, style}) {
+  return (
+    <div style={style}>
+      <img alt={user.name} src={user.img} />
+      <div>
+        <h4>{user.name}</h4>
+        <p>@{user.handle}</p>
+      </div>
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Badge
+      user={USER_DATA}
+      style={{
+        width: 300,
+        margin: "0 auto",
+        border: "1px solid var(--beige-10)",
+        borderRadius: 8,
+        backgroundColor: "var(--charcoal)",
+        padding: 24,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "16px",
+        textAlign: "center"
+      }}
+    />
   );
 }
